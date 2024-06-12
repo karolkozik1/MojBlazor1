96abb5ddd57266b892eff104c98396a8b35d0265
karolkozik1 <1119772@student.san.edu.pl>
2024-06-12 11:32:41 +0200

    Add project files.

---
+++ MojBlazor/CalendarItem.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MojBlazor
+{
+	public record CalendarItem(DateTime Date, string Name);
+
+}
